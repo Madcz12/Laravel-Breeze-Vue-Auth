8654681ca3ed32add3c2118536aaf87b345bfa74
Diamond <diamond12@gmail.com>
2023-10-27 04:54:43 -0400

    agg primer commit, subiendo ambas carpertas asdf

---
+++ vue-breeze-api/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="flex justify-between items-center bg-blue-500 py-2">
+        <router-link :to="{ name: 'Home' }" class="text-white text-xl font-semibold mr-2">Home</router-link>
+        <router-link :to="{ name: 'Login' }" class="text-white text-xl font-semibold mr-2">Login</router-link>
+        <router-link :to="{ name: 'Register' }" class="text-white text-xl font-semibold">Register</router-link>
+    </nav>
+</template>
+
